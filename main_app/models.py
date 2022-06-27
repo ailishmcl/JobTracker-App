74ec04cb3c129321ed90dc138838ce879395900e
Yamilyn <yamilysousa@hotmail.com>
2022-06-27 10:29:20 +0100

    model Jobs

--- main_app/models.py
+++ main_app/models.py
@@ -5,7 +5,24 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+    contract_type = models.CharField(max_length=100)
+    salary = models.CharField(max_length=100)
+    link = models.CharField(max_length=150)
+    description = models.TextField(max_length=250)
+    contact = models.CharField(max_length=150)
+    requirements = models.ManyToManyField(Requirements)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def get_absolute_url(self):
+        return reverse('detail', kwargs = {'job_id': self.id})
+
+    def __str__(self):
+        return self.title
+
+        
 
 
 class Requirements(models.Model):
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Job, Requirements
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+
+
+import os
 
 # Create your views here.
 
+class JobCreate(CreateView):
+    model = Job
+    fields = ['title', 'company', 'contract_type', 'salary', 'link', 'description', 'contact']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+    
+
+class JobUpdate(UpdateView):
+    model = Job
+    fields = ['title', 'company', 'contract_type', 'salary', 'link', 'description', 'contact']
+
+
+class JobDelete(DeleteView):
+    model = Job
+    success_url = '/jobs/'
 
 
 # home URL
 def home(request):
+    os.getenv('NAME')
     return render(request, 'home.html')
