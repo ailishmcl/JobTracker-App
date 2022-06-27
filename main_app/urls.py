7a4078e65b81feeb1b69659567600676eb05c333
ailishmcl <ammclaug@tcd.ie>
2022-06-27 10:27:34 +0100

    added main_app folder to templates and added all html files for requirements. also added views and urls for all pages of requirements

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,10 +6,13 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    
 
 
-
-
+# requirements urls
+    path('requirements/', views.RequirementsList.as_view(), name='requirements_index'),
+    path('requirements/<int:pk>/', views.RequirementsDetail.as_view(), name='requirements_details'),
+    path('requirements/create', views.RequirementsCreate.as_view(), name='requirements_create'),
+    path('requirements/<int:pk>/update/', views.RequirementsUpdate.as_view(), name='requirements_update'),
+    path('requirements/<int:pk>/delete/', views.RequirementsDelete.as_view(), name='requirements_delete'),
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Requirements
 # Create your views here.
 
 
@@ -7,3 +9,27 @@ from django.shortcuts import render
 # home URL
 def home(request):
     return render(request, 'home.html')
+
+
+
+
+
+
+
+class RequirementsList(ListView):
+    model = Requirements
+
+class RequirementsDetail(DetailView):
+    model = Requirements
+
+class RequirementsCreate(CreateView):
+    model = Requirements
+    fields = '__all__'
+
+class RequirementsUpdate(UpdateView):
+    model = Requirements
+    fields = '__all__'
+
+class RequirementsDelete(DeleteView):
+    model = Requirements
+    success_url = '/requirements/'
