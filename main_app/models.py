677461f18bf1a8fe36dfa4433c6313b05eac2bbd
ailishmcl <ammclaug@tcd.ie>
2022-06-27 12:07:41 +0100

    completed requirements html pages, working fine

--- main_app/models.py
+++ main_app/models.py
@@ -5,6 +5,19 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
+
+class Requirements(models.Model):
+    type = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.type
+    
+    def get_absolute_url(self):
+        return reverse('requirements_detail', kwargs = {'pk' : self.id})
+
+
 class Job(models.Model):
     title = models.CharField(max_length=100)
     company = models.CharField(max_length=100)
@@ -24,11 +37,4 @@ class Job(models.Model):
 
         
 
-
-class Requirements(models.Model):
-    type = models.CharField(max_length=100)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.type
     
