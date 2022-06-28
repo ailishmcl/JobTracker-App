071f05acb0e59f48b031dc3b7374cb4542647161
ailishmcl <ammclaug@tcd.ie>
2022-06-28 16:05:34 +0100

    solved profile issue

--- main_app/apps.py
+++ main_app/apps.py
@@ -5,9 +5,10 @@ class MainAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main_app'
 
-class UsersConfig(AppConfig):
-    name = 'users'
+# class UserConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'users'
 
     def ready(self):
         print("testing")
-        return users.signals
+        import main_app.signals
